6153b871cd62baf4db2d8f48e0dc39164f784f1c
Andres2D <andres2d1997.1206@gmail.com>
2022-05-20 11:22:51 -0500

    Add click event handler

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,22 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({title, date, amount}) => {
+
+  let newTitle = title;
+
+  const clickHandler = () => {
+    console.log('Clicked!?!?!?');
+    newTitle = 'Test';
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className='expense-item__price'>${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   )
 }
