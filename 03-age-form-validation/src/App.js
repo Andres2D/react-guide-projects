dd66e6dd09805531832225608d93ce1b23000315
Andres2D <andres2d1997.1206@gmail.com>
2022-05-25 17:08:52 -0500

    Refactor components usage

--- 03-age-form-validation/src/App.js
+++ 03-age-form-validation/src/App.js
@@ -3,38 +3,12 @@ import styles from './App.module.css';
 import AgeForm from './components/AgeForm';
 import AgeRecords from './components/AgeRecords';
 import Card from './UI/Card';
-import Modal from './UI/Modal';
 
 const App = () => {
-
-  const [modal, setModal] = useState({
-    open: false,
-    message: 'This is the defualt message'
-  });
-
   const [users, setUsers] = useState([]); 
 
   const formHandler = (form) => {
-    if(form.user.trim().length === 0 || form.age.trim().length === 0) {
-      setModal({
-        open: true,
-        message: 'Please enter a valid name and age (non-empty values).'
-      });
-    }else if(form.age < 1) {
-      setModal({
-        open: true,
-        message: 'Please enter a valid age (>0).'
-      });
-    }else {
-      setUsers(prevValue => [form, ...prevValue]);
-    }
-  }
-
-  const closeHandler = () => {
-    setModal({
-      open: false,
-      message: ''
-    });
+    setUsers(prevValue => [form, ...prevValue]);
   }
 
   const records = users.map(obj => (
@@ -45,28 +19,14 @@ const App = () => {
   ))
 
   return (
-    <div>
-      <div className={styles.app}>
-        <AgeForm onSubmitForm={formHandler} />
-        {
-          users.length > 0 &&
-          <Card>
-            {records}
-          </Card>
-        }
-      </div>
+    <div className={styles.app}>
+      <AgeForm onSubmitForm={formHandler} />
       {
-          modal.open && 
-          <Modal
-            onCloseModal={closeHandler}>
-            <h3>{modal.message}</h3>  
-            <button 
-              onClick={closeHandler} 
-              className={styles.btn}>
-              Okay
-            </button>
-          </Modal>
-        }
+        users.length > 0 &&
+        <Card>
+          {records}
+        </Card>
+      }
     </div>
   );
 }
--- 03-age-form-validation/src/components/AgeForm.js
+++ 03-age-form-validation/src/components/AgeForm.js
@@ -1,5 +1,7 @@
 import {useState} from 'react';
+import Button from '../UI/Butotn';
 import Card from '../UI/Card';
+import Modal from '../UI/Modal';
 import styles from './AgeForm.module.css';
 
 const AgeForm = props => {
@@ -7,6 +9,11 @@ const AgeForm = props => {
   const [userForm, setUserForm] = useState({
     user: '',
     age: ''
+  });
+
+  const [modal, setModal] = useState({
+    open: false,
+    message: 'This is the defualt message'
   });
 
   const userChangeHandler = (evt) => {
@@ -21,39 +28,78 @@ const AgeForm = props => {
     });
   }
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    props.onSubmitForm(userForm);
-    setUserForm({
-      user: '',
-      age: ''
+  const closeHandler = () => {
+    setModal({
+      open: false,
+      message: ''
     });
   }
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if(userForm.user.trim().length === 0 || userForm.age.trim().length === 0) {
+      setModal({
+        open: true,
+        message: 'Please enter a valid name and age (non-empty values).'
+      });
+    }else if(userForm.age < 1) {
+      setModal({
+        open: true,
+        message: 'Please enter a valid age (>0).'
+      });
+    }else {
+      props.onSubmitForm(userForm);
+      setUserForm({
+        user: '',
+        age: ''
+      });
+    }
+  }
+
   return (
-    <Card>
-      <form onSubmit={handleSubmit}>
-        <div className={styles['form-control']}>
-          <label className={styles.label}>Username</label>
-          <input 
-            className={styles.input} 
-            type='text'
-            value={userForm.user}
-            onChange={userChangeHandler} 
-            />
-        </div>
-        <div className={styles['form-control']}>
-          <label className={styles.label}>Age (Years)</label>
-          <input 
-            className={styles.input} 
-            type='number' 
-            value={userForm.age}
-            onChange={ageChangeHandler}
-            />
-        </div>
-        <button type='submit' className={styles.btn}>Add User</button>
-      </form>
-    </Card>
+    <div className={styles.wrapper}>
+      <Card>
+        <form onSubmit={handleSubmit}>
+          <div className={styles['form-control']}>
+            <label htmlFor='userName' className={styles.label}>Username</label>
+            <input 
+              id='userName'
+              className={styles.input} 
+              type='text'
+              value={userForm.user}
+              onChange={userChangeHandler} 
+              />
+          </div>
+          <div className={styles['form-control']}>
+            <label htmlFor='age' className={styles.label}>Age (Years)</label>
+            <input 
+              id='age'
+              className={styles.input} 
+              type='number' 
+              value={userForm.age}
+              onChange={ageChangeHandler}
+              />
+          </div>
+          <Button
+            onClick={handleSubmit}
+            type='submit'>
+              Add User
+          </Button>
+        </form>
+      </Card>
+      {
+        modal.open && 
+        <Modal
+          onCloseModal={closeHandler}>
+          <h3>{modal.message}</h3>  
+          <Button
+            className={styles.btn}
+            onClick={closeHandler}>
+              Okay
+          </Button>
+        </Modal>
+      }
+    </div>
   )
 }
 
