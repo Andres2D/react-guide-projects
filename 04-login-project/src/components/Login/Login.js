0e4305429fda8e24ab49c92763c15ea22fabbcbe
Andres2D <andres2d1997.1206@gmail.com>
2022-05-27 12:00:18 -0500

    Add debounce time handler to mannage the input events

--- 04-login-project/src/components/Login/Login.js
+++ 04-login-project/src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    // debounce time input handler
+    const handler = setTimeout(() => {
+      console.log('asd');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    
+    return () => {
+      clearTimeout(handler);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
