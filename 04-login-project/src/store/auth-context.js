79dff3353817a5bd92ce10dec09c55eb564c81a7
Andres2D <andres2d1997.1206@gmail.com>
2022-06-02 17:03:31 -0500

    Configure auth context to mange all the login and logout behavior

--- 04-login-project/src/store/auth-context.js
+++ 04-login-project/src/store/auth-context.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const AuthContext = React.createContext({
-  isLoggedIn: false
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLogedInfo = localStorage.getItem('isLoggedIn');
+    if(storedUserLogedInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+  }
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
