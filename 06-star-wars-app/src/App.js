de7afa2f9c0105cea249bc15b062499651a59c17
Andres2D <andres2d1997.1206@gmail.com>
2022-06-16 10:20:33 -0500

    Handle fetch errors

--- 06-star-wars-app/src/App.js
+++ 06-star-wars-app/src/App.js
@@ -7,22 +7,46 @@ function App() {
 
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   
   const fetcthMovieshandler = async() => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    console.log(response);
-    const data = await response.json();
-    const transformedmovies = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date
-      };
-    });
-    setMovies(transformedmovies);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+
+      if(!response.ok){
+        throw new Error('Somenthing went wrong!');
+      }
+      
+      const data = await response.json();
+      const transformedmovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        };
+      });
+      setMovies(transformedmovies);
+    }catch(err) {
+      setError(err.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Find no movies</p>;
+
+  if(movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if(error) {
+    content = <p>{error}</p>
+  }
+
+  if(isLoading) {
+    content = <p>Loading ...</p>
   }
 
   return (
@@ -31,9 +55,7 @@ function App() {
         <button onClick={fetcthMovieshandler}>Fetch Movies</button>
       </section>
       <section>
-        { !isLoading && movies.length > 0 && <MoviesList movies={movies} /> }
-        { !isLoading && movies.length === 0 && <p>Find no movies</p>}
-        { isLoading && <p>Loading ...</p>}
+        { content }
       </section>
     </React.Fragment>
   );
