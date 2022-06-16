af789ea54e2471b0ba089a99250ed67f0cf542f1
Andres2D <andres2d1997.1206@gmail.com>
2022-06-16 10:35:54 -0500

    Sent request when the component inits

--- 06-star-wars-app/src/App.js
+++ 06-star-wars-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   
-  const fetcthMovieshandler = async() => {
+  const fetcthMovieshandler = useCallback(async() => {
     setIsLoading(true);
     setError(null);
     try {
@@ -33,7 +33,11 @@ function App() {
       setError(err.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetcthMovieshandler();
+  }, [fetcthMovieshandler]);
 
   let content = <p>Find no movies</p>;
 
