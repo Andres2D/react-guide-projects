31200d81b7151b4f7f919443e07cd6ab1f486e19
Andres2D <andres2d1997.1206@gmail.com>
2022-06-22 16:58:35 -0500

    Add a new slicer for auth reducer

--- 11-react-redux/src/store/index.js
+++ 11-react-redux/src/store/index.js
@@ -1,14 +1,17 @@
-import { createStore } from 'redux';
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true
 };
 
+const initialAuthSlice = {
+  isAutheticated: false
+}
+
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -25,9 +28,26 @@ const counterSlice = createSlice({
   }
 });
 
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthSlice,
+  reducers: {
+    login(state) {
+      state.isAutheticated = true
+    },
+    logout(state) {
+      state.isAutheticated = false
+    }
+  }
+});
+
 const store = configureStore( {
-  reducer: counterSlice.reducer   
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  }   
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
