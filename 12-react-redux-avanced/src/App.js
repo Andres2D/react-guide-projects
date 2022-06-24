cdc5316664b284ddd496d3f1dfb25a64c6a22e57
Andres2D <andres2d1997.1206@gmail.com>
2022-06-24 15:22:05 -0500

    Create a custom action to fetch initial data from firebase

--- 12-react-redux-avanced/src/App.js
+++ 12-react-redux-avanced/src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Notification from './components/UI/Notification';
 import Products from './components/Shop/Products';
-import { sendCartData } from './store/cart-slice'; 
+import { sendCartData, fetchCartData } from './store/cart-actions'; 
 
 let isInitial = true;
 
@@ -14,6 +14,10 @@ function App() {
   const cart = useSelector(state => state.cart);
   const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if(isInitial) {
