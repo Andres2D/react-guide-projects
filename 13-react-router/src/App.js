2acc51ea1a7ce544a4fc251d60b3d467aa510feb
Andres2D <andres2d1997.1206@gmail.com>
2022-06-25 11:22:03 -0500

    Add link componments to navigate

--- 13-react-router/src/App.js
+++ 13-react-router/src/App.js
@@ -1,18 +1,22 @@
 import { Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
-      <Route 
-        path='/welcome'>
-          <Welcome />
-      </Route>
-      <Route 
-        path='/products'>
-          <Products />
-      </Route>
+      <Header />
+      <main>
+        <Route 
+          path='/welcome'>
+            <Welcome />
+        </Route>
+        <Route 
+          path='/products'>
+            <Products />
+        </Route>
+      </main>
     </div>
   );
 }
