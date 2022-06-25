bc71373721bae276152e317d5d1c9625d84bbcaf
Andres2D <andres2d1997.1206@gmail.com>
2022-06-25 13:02:02 -0500

    Add quote list and quote form components

--- 14-quotes-app-router-practice/src/pages/Quotes.js
+++ 14-quotes-app-router-practice/src/pages/Quotes.js
@@ -1,5 +1,20 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'Learning React is Fun!'
+  },
+  {
+    id: 'q2',
+    author: 'Maximilian',
+    text: 'Learning React is Great!'
+  }
+];
+
 const Quotes = () => {
-  return <h1>Quotes page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default Quotes;
