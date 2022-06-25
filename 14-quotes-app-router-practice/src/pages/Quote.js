10487b9f7c43c9b88531c3d89c057392bd476f65
Andres2D <andres2d1997.1206@gmail.com>
2022-06-25 13:12:13 -0500

    refactor quote page

--- 14-quotes-app-router-practice/src/pages/Quote.js
+++ 14-quotes-app-router-practice/src/pages/Quote.js
@@ -1,13 +1,33 @@
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighLightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'Learning React is Fun!'
+  },
+  {
+    id: 'q2',
+    author: 'Maximilian',
+    text: 'Learning React is Great!'
+  }
+];
 
 const Quote = () => {
   const params = useParams();
-  console.log(params);
+  const quote = DUMMY_QUOTES.find(q => q.id === params.quoteId);
+
+  if(!quote) {
+    return (
+      <p>No quote found!</p>
+    );
+  }
+
   return (
     <>
-      <h1>Quotes page</h1>
-      <p>{params.quoteId}</p>
+      <HighLightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
