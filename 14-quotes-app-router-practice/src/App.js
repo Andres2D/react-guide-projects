2edbf6cdd144d1a0f02f4c14f66c371ed49e4157
Andres2D <andres2d1997.1206@gmail.com>
2022-06-25 12:55:53 -0500

    Add layout component with nav links

--- 14-quotes-app-router-practice/src/App.js
+++ 14-quotes-app-router-practice/src/App.js
@@ -2,25 +2,28 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Quote from './pages/Quote';
 import Quotes from './pages/Quotes';
 import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <Quotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <Quote />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-      </Switch>
-    </div>
+    <Layout>
+      <div>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <Quotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <Quote />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+        </Switch>
+      </div>
+    </Layout>
   );
 }
 
