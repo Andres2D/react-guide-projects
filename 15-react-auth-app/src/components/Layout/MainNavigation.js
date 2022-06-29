1d991fdaf1a196a93272e07538c89fb773a6d074
Andres2D <andres2d1997.1206@gmail.com>
2022-06-28 21:47:10 -0500

    Add logut feature

--- 15-react-auth-app/src/components/Layout/MainNavigation.js
+++ 15-react-auth-app/src/components/Layout/MainNavigation.js
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useContext } from 'react';
 
 import classes from './MainNavigation.module.css';
 import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+  const history = useHistory();
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    history.replace('/auth');
+  };
 
   return (
     <header className={classes.header}>
@@ -26,7 +33,7 @@ const MainNavigation = () => {
                 <Link to='/profile'>Profile</Link>
               </li>
               <li>
-                <button>Logout</button>
+                <button onClick={logoutHandler}>Logout</button>
               </li>
             </>
           )}
