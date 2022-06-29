e7a78250221a10e8355ce81d2361d90204604b9b
Andres2D <andres2d1997.1206@gmail.com>
2022-06-28 22:21:46 -0500

    Add expiration time persisntent

--- 15-react-auth-app/src/store/auth-context.js
+++ 15-react-auth-app/src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
   toke: '',
@@ -16,24 +18,62 @@ const calculateRemainingTime = (expirationTime) => {
   return remainingDuration;
 };
 
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationTime = localStorage.getItem('expirationTime');
+
+  const remainingTime = calculateRemainingTime(storedExpirationTime);
+
+  if(remainingTime <= 60000) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    return null;
+  } 
+
+  return {
+    token: storedToken,
+    duration: remainingTime
+  }
+};
+
 export const AuthContextProvider = props => {
-  const initialToken = localStorage.getItem('token');
+  let initialToken;
+  const tokenData = retrieveStoredToken();
+
+  if(tokenData) {
+    initialToken = tokenData.token;
+  }
+
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+
+    if(logoutTimer) {
+      console.log('inn');
+      clearTimeout(logoutTimer);
+    }
   };
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem('token', token);
+    localStorage.setItem('expirationTime', expirationTime);
 
     const remainingTime = calculateRemainingTime(expirationTime);
 
-    setTimeout(logoutHandler, remainingTime);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+
+  useEffect(() => {
+    if(tokenData) {
+      console.log(tokenData.duration);
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  }, [tokenData]);
 
   const contextValue = {
     token,
