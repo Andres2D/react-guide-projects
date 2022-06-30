dd8ecd456fbb01f12a6667788aa4379ff1e8a5a4
Andres2D <andres2d1997.1206@gmail.com>
2022-06-30 12:08:29 -0500

    Linking pages

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,6 +1,21 @@
+import Link from 'next/link';
 
 const NewsPage = () => {
-  return <h1>The News Page</h1>
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/js-for-you">JS for you</Link>
+        </li>
+      </ul>
+    </>
+  )
 };
 
 export default NewsPage;
