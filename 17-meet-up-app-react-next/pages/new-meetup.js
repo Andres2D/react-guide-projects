09b55538eb7184f79fbee2686502d0cd60417bd1
Andres2D <andres2d1997.1206@gmail.com>
2022-07-01 16:59:20 -0500

    Add api integration with page to add a new meetup

--- 17-meet-up-app-react-next/pages/new-meetup.js
+++ 17-meet-up-app-react-next/pages/new-meetup.js
@@ -1,9 +1,22 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
 
-  const addMeetupHandler = (form) => {
-    console.log(form);
+  const router = useRouter();
+
+  const addMeetupHandler = async(form) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body:JSON.stringify(form),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
