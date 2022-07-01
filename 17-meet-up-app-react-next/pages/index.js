3d25f5cd16caa2f32052fe863ba38ff56c26a762
Andres2D <andres2d1997.1206@gmail.com>
2022-07-01 17:08:01 -0500

    Fetching data from DB in the getStaticProps method

--- 17-meet-up-app-react-next/pages/index.js
+++ 17-meet-up-app-react-next/pages/index.js
@@ -1,28 +1,5 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://thumbs.dreamstime.com/b/shanghai-morning-city-landscape-empty-asphalt-road-shanghai-morning-city-landscape-asphalt-road-155904083.jpg',
-    address: 'Shangai 1334543',
-    description: 'This is a first meetup'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://thumbs.dreamstime.com/b/shanghai-morning-city-landscape-empty-asphalt-road-shanghai-morning-city-landscape-asphalt-road-155904083.jpg',
-    address: 'Shangai 1334543',
-    description: 'This is a second meetup'
-  },
-  {
-    id: 'm3',
-    title: 'A Third Meetup',
-    image: 'https://thumbs.dreamstime.com/b/shanghai-morning-city-landscape-empty-asphalt-road-shanghai-morning-city-landscape-asphalt-road-155904083.jpg',
-    address: 'Shangai 1334543',
-    description: 'This is a third meetup'
-  }
-]
 
 const HomePage = props => {
   return <MeetupList meetups={props.meetups} /> 
@@ -44,9 +21,22 @@ const HomePage = props => {
 // Pregenerated during tht build proccess
 export const getStaticProps = async() => {
   // fetch data from API
+  const client = await MongoClient.connect(process.env.MONGODB_CNN);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map( meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString()
+      }))
     },
     revalidate: 10 // incremental static generation ---> every 10 seconds, data never older of 10 seconds
   };
