fb44f8f6ad604df6fb87542cc110fbf8abd058af
Andres2D <andres2d1997.1206@gmail.com>
2022-07-07 16:46:35 -0500

    Add test with user interation

--- 20-react-unit-test/src/components/Greeting.test.js
+++ 20-react-unit-test/src/components/Greeting.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
-  test('should renders Hello World', () => {
+
+  test('should render Hello World', () => {
     // Arrange
     render(<Greeting />);
   
@@ -11,5 +13,23 @@ describe('Greeting component', () => {
     // Assert
     const titleElement = screen.getByText('Hello world!', {exact: true});
     expect(titleElement).toBeInTheDocument();
+  });
+
+  test('should render good to see you if the button was NOT clicked', () => {
+    render(<Greeting />);
+    const longText = screen.getByText("It's good to see you!", {exact: false});
+    expect(longText).toBeInTheDocument();
+  });
+  
+  test('should reder Changed! if the button was clicked', () => {
+    render(<Greeting />);
+    
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+    const changeText = screen.getByText("Changed!", {exact: false});
+    const longText = screen.queryByText("It's good to see you!", {exact: false});
+    expect(changeText).toBeInTheDocument();
+    expect(longText).toBeNull();
   });
 });
