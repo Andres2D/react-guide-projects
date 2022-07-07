beef27db834230cd8f73fbf82a216158e5412c5a
Andres2D <andres2d1997.1206@gmail.com>
2022-07-07 16:28:56 -0500

    Agrupoing the test into a suit case test

--- 20-react-unit-test/src/components/Greeting.test.js
+++ 20-react-unit-test/src/components/Greeting.test.js
@@ -1,13 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('should renders Hello World', () => {
-  // Arrange
-  render(<Greeting />);
-
-  // Act, ...nothiung
-
-  // Assert
-  const titleElement = screen.getByText('Hello world!', {exact: true});
-  expect(titleElement).toBeInTheDocument();
+describe('Greeting component', () => {
+  test('should renders Hello World', () => {
+    // Arrange
+    render(<Greeting />);
+  
+    // Act, ...nothiung
+  
+    // Assert
+    const titleElement = screen.getByText('Hello world!', {exact: true});
+    expect(titleElement).toBeInTheDocument();
+  });
 });
