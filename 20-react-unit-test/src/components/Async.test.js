ce84ca19b077c91809a3efd86c01dc501f6fc49a
Andres2D <andres2d1997.1206@gmail.com>
2022-07-07 17:10:20 -0500

    Add testing async function with mock value on fetch function

--- 20-react-unit-test/src/components/Async.test.js
+++ 20-react-unit-test/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from './Async';
 
 describe('Asyn component', () => {
   test('render post if request succeeds', async() => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async() => [{id: 'p1', title: 'One'}, {id: 'p2', title: 'Two'}, {id: 'p3', title: 'Three'}]
+    });
     render(<Async />);
     const listItems = await screen.findAllByRole('listitem');
     expect(listItems).not.toHaveLength(0);
