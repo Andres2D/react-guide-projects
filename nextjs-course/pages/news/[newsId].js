abe683c8f6b61a464a8eaddfdedad444e7f9b622
Andres2D <andres2d1997.1206@gmail.com>
2022-07-15 11:48:42 -0500

    Add different folder structure to project

--- nextjs-course/pages/news/[newsId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const DetailPage = () => {
-  const { query } = useRouter();
-  const newsId = query.newsId;
-  return <h1>The Detail Page: {newsId}</h1>;
-};
-
-export default DetailPage;
