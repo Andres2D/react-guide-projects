ff2b6eae3ea30076f6e174675b2be2a4ee6d926d
Andres2D <andres2d1997.1206@gmail.com>
2022-07-15 13:25:45 -0500

    Getting the query param from userRouter hook

--- nextjs-course/pages/portfolio/[projectId].js
+++ nextjs-course/pages/portfolio/[projectId].js
@@ -1,7 +1,14 @@
+import { useRouter } from 'next/router';
+
 const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
   return (
     <div>
-      <h1>The portfolio Project Page</h1>
+      <h1>The portfolio Project Page [{router.query.projectId}]</h1>
     </div>
   );
 };
