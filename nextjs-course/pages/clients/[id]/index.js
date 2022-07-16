6cb46fa5a5a9aac1318de425aec781ca2a923086
Andres2D <andres2d1997.1206@gmail.com>
2022-07-16 16:39:28 -0500

    navigatin programmatically

--- nextjs-course/pages/clients/[id]/index.js
+++ nextjs-course/pages/clients/[id]/index.js
@@ -1,7 +1,31 @@
+import { useRouter } from 'next/router';
+
 const ClientProjectsPage = () => {
+
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // approach 1
+    // router.push('/clients/max/projectA');
+    
+    // approach 2
+    router.push({
+      pathname: '/clients/max/projectA',
+      query: {
+        id: 'max',
+        clientProjectId: 'projectA'
+      }
+    });
+    
+    //cant go back on navigation
+    // router.replace('/clients/max/projectA');
+
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 };
