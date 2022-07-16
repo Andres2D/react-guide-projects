40219fcaf39aa8e861de570f753f224f1c72e177
Andres2D <andres2d1997.1206@gmail.com>
2022-07-16 16:32:54 -0500

    Alternative to template strings path

--- nextjs-course/pages/clients/index.js
+++ nextjs-course/pages/clients/index.js
@@ -17,7 +17,14 @@ const ClientsPage = () =>  {
     <div>
       <h1>The Clients Page</h1>
       <ul>
-        {clients.map(client => <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link></li>)}
+        {clients.map(client => (
+          <li key={client.id}><Link href={{
+            pathname: '/clients/[id]',
+            query: {
+              id: client.id
+            }
+          }}>{client.name}</Link></li>)
+        )}
       </ul>
     </div> 
   );
