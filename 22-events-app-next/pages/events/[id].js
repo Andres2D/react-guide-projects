0b36d31f25440ebceb7e8000b1433cdf97e45ea4
Andres2D <andres2d1997.1206@gmail.com>
2022-07-19 15:02:28 -0500

    Add event details page components

--- 22-events-app-next/pages/events/[id].js
+++ 22-events-app-next/pages/events/[id].js
@@ -1,12 +1,33 @@
 import { useRouter } from 'next/router';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventContent from '../../components/event-detail/event-content';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import { getEventById } from '../../dummy_data';
 
 const EventDetailPage = () => {
   const router = useRouter();
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if(!event) {
+    return <p>No event found!</p>
+  }
 
   return (
-    <div>
-      <h1>The Event Page: [{router.query.id}]</h1>
-    </div>
+    <>
+      <EventSummary
+        title={event.title}
+      />
+      <EventLogistics 
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   )
 };
 
