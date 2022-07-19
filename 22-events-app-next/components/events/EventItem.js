3eed71625500eefd9746760be8347e44115e9684
Andres2D <andres2d1997.1206@gmail.com>
2022-07-19 14:52:13 -0500

    Add icons and button components

--- 22-events-app-next/components/events/EventItem.js
+++ 22-events-app-next/components/events/EventItem.js
@@ -1,4 +1,7 @@
-import Link from 'next/link';
+import Button from '../ui/Button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 import styles from './EventItem.module.css';
 
 const EventItem = props => {
@@ -19,14 +22,19 @@ const EventItem = props => {
         <div className={styles.summary}>
           <h2>{title}</h2>
           <div className={styles.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={styles.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={styles.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}><ArrowRightIcon /></span>
+          </Button>
         </div>
       </div>
     </li>
