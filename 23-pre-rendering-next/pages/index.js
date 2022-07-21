ac764d76a7813a97e3d35023c62c39229b5786da
Andres2D <andres2d1997.1206@gmail.com>
2022-07-21 16:41:00 -0500

    Use fs to prerender data from dummy json on server side

--- 23-pre-rendering-next/pages/index.js
+++ 23-pre-rendering-next/pages/index.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import fs from 'fs/promises';
+
 const HomePage = props => {
 
   const {products} = props;
@@ -11,9 +14,15 @@ const HomePage = props => {
 
 // Server side code
 export const getStaticProps = async () => {
+
+  // process.cwd() --> get current working directory
+  const filepath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filepath);
+  const data = JSON.parse(jsonData);
+
   return { 
     props: {
-      products: [{id: 'p1', title: 'Product 1'}]
+      products: data.products
     } 
   };
 }
