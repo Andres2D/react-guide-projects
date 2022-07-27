f1fe4cb1ab61229ebda288ee4fcc93387349b737
Andres2D <andres2d1997.1206@gmail.com>
2022-07-26 19:44:16 -0500

    Add properties to handle errors in the getStaticProps function

--- 23-pre-rendering-next/pages/index.js
+++ 23-pre-rendering-next/pages/index.js
@@ -13,17 +13,32 @@ const HomePage = props => {
 }
 
 // Server side code
-export const getStaticProps = async () => {
-
+export const getStaticProps = async (context) => {
+  console.log('Regenerating ...');
   // process.cwd() --> get current working directory
   const filepath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filepath);
   const data = JSON.parse(jsonData);
 
+  if(!data) {
+    return {
+      redirect: {
+        destination: '/no-data' // Redirecting to another page in case of API fails
+      }
+    }
+  }
+
+  if(data.products.length === 0) {
+    return {
+      notFound: true // Returning a complete 404 page if there is any problem
+    };
+  }
+
   return { 
     props: {
       products: data.products
-    } 
+    },
+    revalidate: 10, // every seconds to regenerate the page again
   };
 }
 
