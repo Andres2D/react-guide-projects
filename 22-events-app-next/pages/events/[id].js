20519d7c9380094c633cd35274ef61cd6abdddae
Andres2D <andres2d1997.1206@gmail.com>
2022-08-08 09:49:54 -0500

    prerendering event detail page with getStaticProps and getStaticPaths

--- 22-events-app-next/pages/events/[id].js
+++ 22-events-app-next/pages/events/[id].js
@@ -1,14 +1,11 @@
-import { useRouter } from 'next/router';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import ErrorAlert from '../../components/ui/error-alert';
-import { getEventById } from '../../dummy_data';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 
-const EventDetailPage = () => {
-  const router = useRouter();
-  const eventId = router.query.id;
-  const event = getEventById(eventId);
+const EventDetailPage = props => {
+  const event = props.event;
 
   if(!event) {
     
@@ -35,6 +32,28 @@ const EventDetailPage = () => {
       </EventContent>
     </>
   )
+};
+
+export const getStaticProps = async(context) => {
+  const eventId = context.params.id;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event
+    }
+  }
+};
+
+export const getStaticPaths = async() => {
+  const events = await getAllEvents();
+
+  const paths = events.map(e => ({ params: { id: e.id }}));
+
+  return {
+    paths,
+    fallback: false
+  };
 };
 
 export default EventDetailPage;
