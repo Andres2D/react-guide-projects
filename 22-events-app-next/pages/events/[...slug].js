f8b0d3df6bb13bf37c80362e3f9178ab32805c11
Andres2D <andres2d1997.1206@gmail.com>
2022-08-09 10:26:41 -0500

    Add static generation on slug page

--- 22-events-app-next/pages/events/[...slug].js
+++ 22-events-app-next/pages/events/[...slug].js
@@ -1,64 +1,85 @@
-import { useRouter } from 'next/router';
 import EventList from '../../components/events/EventList';
 import ResultsTitle from '../../components/events/ResultsTitle';
 import Button from '../../components/ui/Button';
 import ErrorAlert from '../../components/ui/error-alert';
-import { getFilteredEvents } from '../../dummy_data';
+import { getFilteredEvents } from '../../helpers/api-util';
 
-const FilteredEventsPage = () => {
-
-  const router = useRouter();
-
-  if(!router.query.slug){
-    return <p className='center'>Loading...</p>;
-  }else{
-    const [year, month] = router.query.slug;
+const FilteredEventsPage = props => {
     
-    if(
-      isNaN(+year) || 
-      isNaN(+month) || 
-      +year > 2030 || 
-      +year < 2021 || 
-      +month < 1 || 
-      +month > 12
-    ) {
-      return (
-        <>
-          <ErrorAlert>
-            <p className='center'>Invalid filter, please adjust your values!</p>
-          </ErrorAlert>
-          <div className='center'>
-            <Button link='/events'>Show All Events</Button>
-          </div>
-        </>
-      )
-    }
-
-    const events = getFilteredEvents({year: +year, month: +month});
-
-    if(!events || events.length === 0) {
-      return (
-        <>
-          <ErrorAlert>
-            <p className='center'>No events Found for the chosen filter!</p>
-          </ErrorAlert>
-          <div className='center'>
-            <Button link='/events'>Show All Events</Button>
-          </div>
-        </>
-      );
-    }
-
-    const date = new Date(year, month - 1);
-
+  if(props.hasError) {
     return (
-      <div>
-        <ResultsTitle date={date} />
-        <EventList items={events} />
-      </div>
+      <>
+        <ErrorAlert>
+          <p className='center'>Invalid filter, please adjust your values!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
     )
   }
 
+  const events = props.events;
+
+  if(!events || events.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p className='center'>No events Found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(props.date.year, props.date.month - 1);
+
+  return (
+    <div>
+      <ResultsTitle date={date} />
+      <EventList items={events} />
+    </div>
+  )
+  
+};
+
+export const getServerSideProps = async(context) => {
+  const { params } = context;
+
+  const [year, month] = params.slug;
+    
+  if(
+    isNaN(+year) || 
+    isNaN(+month) || 
+    +year > 2030 || 
+    +year < 2021 || 
+    +month < 1 || 
+    +month > 12
+  ) {
+    return {
+      props: {
+        hasError: true
+      }
+      // notFound: true,
+      // redirect: {
+      //   destination: '/error-page'
+      // }
+    };
+  }
+
+  const events = await getFilteredEvents({year: +year, month: +month});
+
+  return {
+    props: {
+      events,
+      date: {
+        year,
+        month
+      }
+    }
+  }
 };
 
 export default FilteredEventsPage;
