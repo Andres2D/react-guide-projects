47434c5f68fccfe8d1e40a86ecd1ae354c409f26
Andres2D <andres2d1997.1206@gmail.com>
2022-08-10 11:47:06 -0500

    Add a basic form to post data to the api

--- 24-next-api-routes/pages/index.js
+++ 24-next-api-routes/pages/index.js
@@ -1,7 +1,34 @@
+import { useRef } from 'react'
+
 const HomePage = () => {
+
+  const emailInput = useRef();
+  const feedbackInput = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+    const email = emailInput.current.value;
+    const feedback = feedbackInput.current.value;
+    console.log({
+      emailInput: emailInput.current.value,
+      feedbackInput: feedbackInput.current.value,
+    });
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input ref={emailInput} type='email' id='email' />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea ref={feedbackInput} id='feedback' rows='5'></textarea>
+        </div>
+        <button type='submit'>Send feedback</button>
+      </form>
     </div>
   )
 };
