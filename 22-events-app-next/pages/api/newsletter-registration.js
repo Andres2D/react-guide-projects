691c798951dd7f0382d6c1b4fac3d42c459db1c3
Andres2D <andres2d1997.1206@gmail.com>
2022-08-13 21:32:22 -0500

    Add catching error on the newsletter enpoint

--- 22-events-app-next/pages/api/newsletter-registration.js
+++ 22-events-app-next/pages/api/newsletter-registration.js
@@ -1,5 +1,15 @@
 import { MongoClient } from 'mongodb';
 
+const connectDatabase = async() => {
+  const client = await MongoClient.connect(process.env.MONGO_CONN)
+  return client;
+}
+
+const insertDocument = async(client, document) => {
+  const db = client.db();
+  await db.collection('newsletter').insertOne(document);
+};
+
 const handler = async(req, res) => {
   const { email } = req.body;
   if(!email || !email.includes('@')) {
@@ -8,10 +18,22 @@ const handler = async(req, res) => {
   }
 
   if(req.method === 'POST') {
-    const client = await MongoClient.connect(process.env.MONGO_CONN)
-    const db = client.db();
-    await db.collection('newsletter').insertOne({email});
-    client.close();
+
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the DB failed!'});
+      return;
+    }
+
+    try {
+      await insertDocument(client, {email}); 
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!'});
+      return;
+    }
     res.status(201).json({message: `${email} registered successfully`});
   }
 };
