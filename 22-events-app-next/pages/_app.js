855a9f05352d73bdff274833fbbda013e573f5de
Andres2D <andres2d1997.1206@gmail.com>
2022-08-16 10:05:00 -0500

    Add notification component

--- 22-events-app-next/pages/_app.js
+++ 22-events-app-next/pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import '../styles/globals.css'
+import Notification from '../components/ui/Notification';
 import Layout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='initial-scale=1.0, width=device-width'></meta>
       </Head>
       <Component {...pageProps} />
+      <Notification title='Test' message='This is a test' status='error' />
     </Layout>
   );
 }
