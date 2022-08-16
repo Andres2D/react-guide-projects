72250aaa097d3153e92532176e0b3deb9a64e84d
Andres2D <andres2d1997.1206@gmail.com>
2022-08-16 10:16:05 -0500

    Add notification context actions

--- 22-events-app-next/store/notification-context.js
+++ 22-events-app-next/store/notification-context.js
@@ -1,14 +1,31 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status }
-  showNotification: () => {},
+  showNotification: (notification) => {},
   hideNotification: () => {}
 });
 
 export const NotificationContextProvider = (props) => {
+
+  const [activeNotification, setActiveNotification] = useState();
+
+  const showNotificationHandler = (notification) => {
+    setActiveNotification(notification);
+  };
+  
+  const hideNotificationHandler = () => {
+    setActiveNotification(null);
+  };
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   ) 
