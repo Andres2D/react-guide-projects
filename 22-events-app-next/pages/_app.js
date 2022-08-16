d472c41e04d195340c7b256e8a741d4cb646073a
Andres2D <andres2d1997.1206@gmail.com>
2022-08-16 10:21:18 -0500

    Wrapping the notification context in the layout component

--- 22-events-app-next/pages/_app.js
+++ 22-events-app-next/pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import '../styles/globals.css'
-import Notification from '../components/ui/Notification';
 import { NotificationContextProvider } from '../store/notification-context';
 import Layout from '../components/layout/layout';
 
@@ -12,7 +11,6 @@ function MyApp({ Component, pageProps }) {
           <meta name='viewport' content='initial-scale=1.0, width=device-width'></meta>
         </Head>
         <Component {...pageProps} />
-        <Notification title='Test' message='This is a test' status='error' />
       </Layout>
     </NotificationContextProvider>
   );
