74bb72c280ab760eff1a9c9d74e152b8d2b00b07
Andres2D <andres2d1997.1206@gmail.com>
2022-09-15 20:33:28 -0500

    Set the nextauth api routes

---
+++ 25-next-auth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import NexAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+export default NexAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+        
+        const usersCollection = client.db.collections('users');
+        const user = await usersCollection.findOne({email: credentials.email});
+        
+        if(!user) {
+          throw new Error('No user found!');
+        }
+
+        const isValid = await verifyPassword(credentials.password, user.password);
+
+        if(!isValid) {
+          throw new Error('Could not login!');
+        }
+
+        client.close();
+        
+        return {
+          email: user.email
+        };
+      }
+    })
+  ]
+});
