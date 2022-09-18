6c72cd8cdbfa33483dad9ad898f7f46590b81e63
Andres2D <andres2d1997.1206@gmail.com>
2022-09-18 15:28:16 -0500

    Add frontend implementation of change password

--- 25-next-auth/components/profile/profile-form.js
+++ 25-next-auth/components/profile/profile-form.js
@@ -1,15 +1,33 @@
+import { useRef } from 'react';
 import classes from './profile-form.module.css';
 
-function ProfileForm() {
+function ProfileForm(props) {
+
+  const newPasswordRef = useRef();
+  const oldPasswordRef = useRef();
+
+  const submitHandler = (evt) => {
+    evt.preventDefault();
+    
+    const newPassword = newPasswordRef.current.value;
+    const oldPassword = oldPasswordRef.current.value;
+
+    console.log(newPassword, oldPassword);
+
+    // valid inputs (todo)
+
+    props.onChangePassword({ newPassword, oldPassword });
+  }
+  
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input ref={newPasswordRef} type='password' id='new-password' />
       </div>
       <div className={classes.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input ref={oldPasswordRef} type='password' id='old-password' />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
